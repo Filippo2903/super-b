da9e76be576892de50587eb8284a8d313b10d55b
Filippo <filippo.gentili.06@gmail.com>
2023-02-26 22:52:18 +0100

    add ground pound sprite

--- Player.gd
+++ Player.gd
@@ -4,14 +4,17 @@ const GRAVITY = 3900
 
 const WALK_SPEED = 700
 const RUN_SPEED = 1000
-const GROUND_POUND_SPEED = 1500
-const JUMP_SPEED = 1450
+const GROUND_POUND_SPEED = 1300
+const JUMP_SPEED = 1500
 
 const Direction = {
 	STEADY = 0,
 	LEFT = -1,
 	RIGHT = 1
 }
+
+onready var animation = $AnimatedSprite
+onready var hitbox = $CollisionShape2D
 
 var velocity = Vector2.ZERO
 var direction = Direction.STEADY
@@ -21,33 +24,46 @@ var moving_speed = WALK_SPEED
 var jumping = false
 var ground_pound = false
 
+var sleep = false
+
+var n = 0
+
 
 func animate():
 	if velocity.x != 0:
-		$AnimatedSprite.flip_h = velocity.x < 0
+		animation.flip_h = velocity.x < 0
 	
 	if ground_pound:
-		$AnimatedSprite.play("ground_pound")
-		
+		animation.animation = "ground_pound"
+		animation.stop()
+	
 	elif jumping:
-		$AnimatedSprite.animation = "jump"
-		$AnimatedSprite.stop()
+		animation.animation = "jump"
+		animation.stop()
 	
 	elif velocity.length() > 0.5:
-		if moving_speed <= WALK_SPEED:
-			$AnimatedSprite.play("walk")
+		if abs(velocity.x) > WALK_SPEED:
+			animation.play("run")
 		else:
-			$AnimatedSprite.play("run")
+			animation.play("walk")
 	
 	else:
-		$AnimatedSprite.animation = "steady"
-		$AnimatedSprite.stop()
+		animation.animation = "steady"
+		animation.stop()
+
+func sleep(sec):
+	if n < sec:
+		sleep = true
+	n += 1
 
 func move():
 	direction = Direction.STEADY
 	velocity = move_and_slide(velocity, Vector2.UP)
 	
+	sleep(3)
+	
 	if is_on_floor():
+		animation.set_position(Vector2(hitbox.position.x, hitbox.position.y))
 		jumping = false
 		ground_pound = false
 	
@@ -69,15 +85,19 @@ func move():
 		velocity.y = lerp(velocity.y, 0, 0.4)
 	
 	if Input.is_action_just_pressed("ui_down") and not is_on_floor():
+		hitbox.set_position(Vector2(hitbox.position.x, hitbox.position.y - 20))
 		ground_pound = true
 	
 	if ground_pound:
+		# wait some time before going down
 		velocity.x = 0
 		velocity.y = GROUND_POUND_SPEED
 		return
 	
-	velocity.x = lerp(velocity.x, moving_speed * direction, 0.3)
-	
+	velocity.x = lerp(velocity.x, moving_speed * direction, 0.1)
+
+
+func debug_reset():
 	if Input.is_key_pressed(KEY_R):
 		position.x = 650
 		position.y = 320
@@ -90,5 +110,8 @@ func _physics_process(delta):
 	velocity.y += GRAVITY * delta
 
 func _process(delta):
+	if sleep:
+		return
 	move()
 	animate()
+	debug_reset()
