f34ab8ada95c73a75bd923e74eab9248abcda08c
Riccardo-Spalletta <rikspa14@gmail.com>
2023-03-01 00:16:15 +0100

    add cat

--- Player.gd
+++ Player.gd
@@ -2,12 +2,7 @@ extends KinematicBody2D
 
 const GRAVITY = 3900
 
-<<<<<<< HEAD
 const WALK_SPEED = 500
-=======
-const WALK_SPEED = 700
-const RUN_SPEED = 1000
->>>>>>> da9e76be576892de50587eb8284a8d313b10d55b
 const GROUND_POUND_SPEED = 1300
 const JUMP_SPEED = 1500
 
@@ -23,7 +18,6 @@ onready var hitbox = $CollisionShape2D
 var velocity = Vector2.ZERO
 var direction = Direction.STEADY
 
-<<<<<<< HEAD
 var jumping = false
 var ground_pound = false
 
@@ -31,18 +25,6 @@ var ground_pound_pause = 0
 
 var n = 0
 
-=======
-var moving_speed = WALK_SPEED
-
-var jumping = false
-var ground_pound = false
-
-var sleep = false
-
-var n = 0
-
-
->>>>>>> da9e76be576892de50587eb8284a8d313b10d55b
 func animate():
 	if velocity.x != 0:
 		animation.flip_h = velocity.x < 0
@@ -56,40 +38,19 @@ func animate():
 		animation.stop()
 	
 	elif velocity.length() > 0.5:
-<<<<<<< HEAD
 		animation.play("walk")
-=======
-		if abs(velocity.x) > WALK_SPEED:
-			animation.play("run")
-		else:
-			animation.play("walk")
->>>>>>> da9e76be576892de50587eb8284a8d313b10d55b
 	
 	else:
 		animation.animation = "steady"
 		animation.stop()
 
-<<<<<<< HEAD
 func move(delta):
 	direction = Direction.STEADY
-=======
-func sleep(sec):
-	if n < sec:
-		sleep = true
-	n += 1
-
-func move():
-	direction = Direction.STEADY
-	velocity = move_and_slide(velocity, Vector2.UP)
-	
-	sleep(3)
->>>>>>> da9e76be576892de50587eb8284a8d313b10d55b
 	
 	if is_on_floor():
 		animation.set_position(Vector2(hitbox.position.x, hitbox.position.y))
 		jumping = false
 		ground_pound = false
-<<<<<<< HEAD
 		ground_pound_pause = 0
 		
 	if (Input.is_action_just_pressed("ui_down") or Input.is_action_just_pressed("shift")) and not is_on_floor():
@@ -108,14 +69,6 @@ func move():
 		velocity.y = GROUND_POUND_SPEED
 		velocity = move_and_slide(velocity, Vector2.UP)
 		return
-=======
-	
-	if Input.is_action_just_pressed("shift"):
-		if is_on_floor():
-			moving_speed = RUN_SPEED
-	elif int(velocity.x) == 0:
-		moving_speed = WALK_SPEED
->>>>>>> da9e76be576892de50587eb8284a8d313b10d55b
 	
 	if Input.is_action_pressed("ui_right"):
 		direction += Direction.RIGHT
@@ -128,23 +81,8 @@ func move():
 	elif Input.is_action_just_released("ui_select") and velocity.y < 0:
 		velocity.y = lerp(velocity.y, 0, 0.4)
 	
-<<<<<<< HEAD
 	velocity.x = lerp(velocity.x, WALK_SPEED * direction, 0.1)
 	velocity = move_and_slide(velocity, Vector2.UP)
-=======
-	if Input.is_action_just_pressed("ui_down") and not is_on_floor():
-		hitbox.set_position(Vector2(hitbox.position.x, hitbox.position.y - 20))
-		ground_pound = true
-	
-	if ground_pound:
-		# wait some time before going down
-		velocity.x = 0
-		velocity.y = GROUND_POUND_SPEED
-		return
-	
-	velocity.x = lerp(velocity.x, moving_speed * direction, 0.1)
-
->>>>>>> da9e76be576892de50587eb8284a8d313b10d55b
 
 func debug_reset():
 	if Input.is_key_pressed(KEY_R):
@@ -159,12 +97,6 @@ func _physics_process(delta):
 	velocity.y += GRAVITY * delta
 
 func _process(delta):
-<<<<<<< HEAD
 	move(delta)
-=======
-	if sleep:
-		return
-	move()
->>>>>>> da9e76be576892de50587eb8284a8d313b10d55b
 	animate()
 	debug_reset()
