15515b2c0452e6b889e290b5f6578b74e35b8161
Filippo2903 <filippo.gentili@fermielearning.it>
2023-05-09 09:48:15 +0200

    fix level menu

--- scripts/player_menu.gd
+++ scripts/player_menu.gd
@@ -1,51 +1,43 @@
 extends AnimatedSprite2D
 
-const Direction = {
-	STEADY = 0,
-	LEFT = -1,
-	RIGHT = 1
+const Level = {
+	START = 0,
+	ONE = 1,
+	TWO = 2,
+	BOSS = 3
 }
-var direction = Direction.STEADY
-var distance = 1000
-var destinazione = 0
-var speed = 1000
-var direction_watching
-var livello_selezionato = 0
-var livelli = ["res://scenes/Level_1.tscn","res://scenes/Level_2.tscn"]
 
+const SPEED = 1000
 
-func _ready():
-	pass
-	
+var current_level = Level.START
+var level_selected = Level.START
+var goal = position.x
+
+var distance = [1000, 1020, 1540, 1860]
+var livelli = ["", "res://scenes/Level_1.tscn", "res://scenes/Level_2.tscn", ""]
 
 func animate():
-	if position.x != destinazione:
-		flip_h = direction_watching - 1
+	flip_h = position.x > goal
 	
-	if position.x != destinazione:
+	if position.x != goal:
 		play("walk")
 	else:
+		current_level = level_selected
 		animation = "steady"
 		stop()
 
 func _process(delta):
+	if Input.is_action_just_pressed("ui_right") and current_level == level_selected and level_selected < Level.BOSS:
+		level_selected += 1
+		goal = position.x + distance[level_selected]
 	
-	if position.x == destinazione:
-		
-		if Input.is_action_just_pressed("ui_right"):
-			direction = Direction.RIGHT
-			direction_watching = direction
-		if Input.is_action_just_pressed("ui_left"):
-			direction = Direction.LEFT
-			direction_watching = direction
-		if Input.is_action_just_pressed("ui_accept"):
-			get_tree().change_scene_to_file(livelli[livello_selezionato])
+	if Input.is_action_just_pressed("ui_left") and current_level == level_selected and level_selected > Level.START:
+		level_selected += -1
+		goal = position.x - distance[current_level]
 	
+	if Input.is_action_just_pressed("ui_accept") and current_level == level_selected and level_selected > Level.START:
+		get_tree().change_scene_to_file(livelli[level_selected])
 	
-	if (livello_selezionato + direction >= 0) and (livello_selezionato + direction <= livelli.size()):
-		print(livello_selezionato)
-		livello_selezionato += direction
-		destinazione += direction * distance
-		position.x = move_toward(position.x, destinazione, delta * speed)
+	position.x = move_toward(position.x, goal, delta * SPEED)
+	
 	animate()
-	direction = Direction.STEADY
