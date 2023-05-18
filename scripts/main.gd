4a8963a86eba3bfb200e1c99b34724c8f1b621bb
Riccardo-Spalletta <rikspa14@gmail.com>
2023-05-18 19:40:36 +0200

    options preloading

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,5 +1,29 @@
 extends Node2D
 
+
+
+func _ready():
+	var config = ConfigFile.new()
+	var fullscreen_check_button: CheckButton = $%FullscreenCheckButton
+	var vsync_check_button: CheckButton = $%VSyncCheckButton
+	
+	var err = config.load("res://options.cfg")
+	var fullscreen = config.get_value("Options", "fullscreen", false)
+	var vsync = config.get_value("Options", "vsync", false)
+
+	if fullscreen:
+		if DisplayServer.window_get_mode() != DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+	else:
+		if DisplayServer.window_get_mode() != DisplayServer.WINDOW_MODE_WINDOWED:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+	if vsync:
+			DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ENABLED)
+	else:
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
+		
+	
 func _on_quit_pressed():
 	get_tree().quit()
 
